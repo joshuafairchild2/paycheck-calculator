88ca845891e44cdcbe5df1a1be4014083aec6c85
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-02-13 00:14:10 -0800

    fixed js logic so that it wont return an error when a field is left blank

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
-$(function() {
+ $(function() {
   $("#calculator form").submit(function(event) {
-    var hourlyRateInput = parseFloat($("input#hourly-rate").val(), 10);
-    var hoursInput = parseFloat($("input#normal-hours").val(), 10);
-    var otInput = parseFloat($("input#ot-hours").val(), 10);
-    var dtInput = parseFloat($("input#dt-hours").val(), 10);
+    var hourlyRateInput = $("input#hourly-rate").val();
+    var hoursInput = $("input#normal-hours").val();
+    var otInput = $("input#ot-hours").val();
+    var dtInput = $("input#dt-hours").val();
 
     var otRate = (hourlyRateInput * 1.5);
     var dtRate = (hourlyRateInput * 2);
-
     var grossPay = ((hourlyRateInput * hoursInput) + (otRate * otInput) + (dtRate * dtInput)).toFixed(2);
 
     $(".calculated-gross").text("$" + grossPay);
-
     event.preventDefault();
   });
 });
